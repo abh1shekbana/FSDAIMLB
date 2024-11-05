75d260d1d909bc86013b81fed476f5f7d5a0a4da
abh1shekbana <cocbana3@gmail.com>
2024-11-05 10:27:35 +0530

    JS embed

---
+++ JSWORK/index.js
@@ -0,0 +1,15 @@
+// console.log("Hello JS");
+
+// const a=12;
+// if(a>10){
+//     let b=23;
+//     a=40;
+//     console.log("a="+a);
+// }
+// console.log("Hi, b="+b)
+
+function fun(a,b){
+    console.log("Heeee");
+    return a+b;
+}
+fun();
